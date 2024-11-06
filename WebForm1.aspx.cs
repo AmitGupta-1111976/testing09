9e03739be9c6c2101754da64d6bad26a8395bb5c
Amit Gupta <amit.gupta@guljaginfotech.com>
2024-11-06 11:39:09 +0530

    a=amit

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -9,11 +9,13 @@ namespace testing09
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        string a;
         protected void Page_Load(object sender, EventArgs e)
         {
             //test3
             //test5
             //test 6 
+            a = "amit";
         }
     }
 }
